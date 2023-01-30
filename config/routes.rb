f0086fb35f36f82efe4f359d67c9409246412aac
Lakshya Pandora <Lakshyapandora123@gmail.com>
2023-01-30 16:38:38 +0530

    Comment model is created and associated with Article model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   #get "/articles/:id", to: "articles#show"
   #get 'articles/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
